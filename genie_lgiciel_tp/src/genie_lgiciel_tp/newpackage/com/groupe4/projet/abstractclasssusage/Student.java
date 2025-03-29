318e7f89888d267dd77efac4848f1f1417971754
johnkibeho <isaackibeho66@gmail.com>
2025-03-29 06:58:54 +0100

    Update Student.java
    class person modif

--- genie_lgiciel_tp/src/genie_lgiciel_tp/newpackage/com/groupe4/projet/abstractclasssusage/Student.java
+++ genie_lgiciel_tp/src/genie_lgiciel_tp/newpackage/com/groupe4/projet/abstractclasssusage/Student.java
@@ -1,69 +1,85 @@
-
 package genie_lgiciel_tp.newpackage.com.groupe4.projet.abstractclasssusage;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import genie_lgiciel_tp.newpackage.com.groupe4.projets.connectionsample.ConnectionFactory;
+
 public class Student extends Person {
-	private String rollNumber;
+    private String rollNumber;
 
-	public Student() {
-	}
+    // Constructeur par défaut
+    public Student() {
+    }
 
-	public Student(int id, String firstName, String lastName, String rollNumber) {
-		super(id, firstName, lastName);
-		this.rollNumber = rollNumber;
-	}
+    // Constructeur avec paramètres
+    public Student(int id, String firstName, String lastName, String rollNumber) {
+        super(id, firstName, lastName);
+        this.rollNumber = rollNumber;
+    }
 
-	public String getRollNumber() {
-		return rollNumber;
-	}
+    // Getters et Setters
+    public String getRollNumber() {
+        return rollNumber;
+    }
 
-	public void setRollNumber(String rollNumber) {
-		this.rollNumber = rollNumber;
-	}
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
 
-	@Override
-	public void showIdentity() {
-		System.out.println(String.format("Student with ID [%s], FistName [%s], LastName [%s], Roll Number [%s]", id,
-				firstName, lastName, rollNumber));
-	}
+    /**
+     * Afficher l'identité d'un étudiant sans connexion à la base de données.
+     */
+    @Override
+    public void showIdentity() {
+        System.out.println(String.format("Étudiant avec ID [%d], Prénom [%s], Nom [%s], Numéro de matricule [%s]", 
+                id, firstName, lastName, rollNumber));
+    }
 
-	@Override
-	public int add(Person p) throws SQLException {
-		String sqlQuery = "INSERT INTO student(id,firstName,lastName,rollNumber) VALUES(?,?,?,?)";
-		// Usage of try-with-resources statement to unsure that JDBC resources are
-		// properly closed
-		try (PreparedStatement ps = ConnectionFactory.getConnection(ConnectionFactory.MYSQL_CONNECTION)
-				.prepareStatement(sqlQuery)) {
-			ps.setInt(1, p.getId());
-			ps.setString(2, p.getFirstName());
-			ps.setString(3, p.getLastName());
-			ps.setString(4, ((Student) p).getRollNumber());
+    /**
+     * Ajouter un étudiant dans la base de données.
+     * 
+     * @param p Objet Person (de type Student)
+     * @return Nombre de lignes affectées
+     * @throws SQLException En cas d'erreur avec la base de données.
+     */
+    @Override
+    public int add(Person p) throws SQLException {
+        String sqlQuery = "INSERT INTO student(id, firstName, lastName, rollNumber) VALUES(?, ?, ?, ?)";
 
-			return ps.executeUpdate();
-		}
-	}
+        try (PreparedStatement ps = ConnectionFactory.getConnection(ConnectionFactory.MYSQL_CONNECTION)
+                .prepareStatement(sqlQuery)) {
+            ps.setInt(1, p.getId());
+            ps.setString(2, p.getFirstName());
+            ps.setString(3, p.getLastName());
+            ps.setString(4, ((Student) p).getRollNumber());
 
-	@Override
-	public void showDynamicIdentity(int id) throws SQLException {
-		String sqlQuery = "SELECT student.id, student.firstName, student.lastName, student.rollNumber FROM student WHERE student.id=?";
-		// Usage of try-with-resources statement to unsure that JDBC resources are
-		// properly closed
-		try (PreparedStatement ps = ConnectionFactory.getConnection(ConnectionFactory.MYSQL_CONNECTION)
-				.prepareStatement(sqlQuery)) {
-			ps.setInt(1, id);
-			
-			try (ResultSet rs = ps.executeQuery()) {
-				if (rs.next()) {
-					System.out.println(String.format(
-							"Student with ID [%s], FistName [%s], LastName [%s], Roll Number [%s]", rs.getInt("id"),
-							rs.getString("firstName"), rs.getString("lastName"), rs.getString("rollNumber")));
-				}
-			}
+            return ps.executeUpdate();
+        }
+    }
 
-		}
-	}
+    /**
+     * Récupérer et afficher dynamiquement l'identité d'un étudiant depuis la base de données.
+     * 
+     * @param id L'ID de l'étudiant à rechercher.
+     * @throws SQLException En cas d'erreur avec la base de données.
+     */
+    @Override
+    public void showDynamicIdentity(int id) throws SQLException {
+        String sqlQuery = "SELECT id, firstName, lastName, rollNumber FROM student WHERE id=?";
 
+        try (PreparedStatement ps = ConnectionFactory.getConnection(ConnectionFactory.MYSQL_CONNECTION)
+                .prepareStatement(sqlQuery)) {
+            ps.setInt(1, id);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    System.out.println(String.format("Étudiant avec ID [%d], Prénom [%s], Nom [%s], Numéro de matricule [%s]", 
+                            rs.getInt("id"), rs.getString("firstName"), rs.getString("lastName"), rs.getString("rollNumber")));
+                } else {
+                    System.out.println("⚠️ Aucun étudiant trouvé avec l'ID " + id);
+                }
+            }
+        }
+    }
 }
